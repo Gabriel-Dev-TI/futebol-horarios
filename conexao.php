69694d4442533176538f0bbe390a9e4ec495945a
Gabriel-Dev-TI <atendimentobiel90@gmail.com>
2025-12-15 12:43:31 -0300

    conexão com o banco de dados para o wasmer

--- conexao.php
+++ conexao.php
@@ -1,15 +1,15 @@
 <?php
-/*
+
 $servidor = "db.fr-pari1.bengt.wasmernet.com:10272";
 $Nomebd = "cdfDoFut";
 $usuario = "94c58df3737c80000a8e1976e5d0";
-$senha = "069194c5-8df3-7550-8000-74f0e658215d";*/
+$senha = "069194c5-8df3-7550-8000-74f0e658215d";
 
-
+/*
 $servidor = "localhost:3306";
 $Nomebd = "cdfDoFut";
 $usuario = "root";
-$senha = "";
+$senha = "";*/
 
 $conexao = mysqli_connect($servidor,$usuario,$senha,$Nomebd);
 
